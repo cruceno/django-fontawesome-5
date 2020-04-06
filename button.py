91ea9262df095140e47dd135890e4544426f98f1
Javier Cruceño <cruceno.jm@gmail.com>
2020-04-06 08:51:04 -0300

    Agregada posibilidad de pasar parametros en la accion del boton

--- button.py
+++ button.py
@@ -1,21 +1,5 @@
 from machine import Pin, Timer
 
-# def on_pressed(timer):
-#     print('pressed')
-
-# def debounce(pin):
-    # Start or replace a timer for 200ms, and trigger on_pressed.
-    # timer.init(mode=Timer.ONE_SHOT, period=200, callback=on_pressed)
-
-# Register a new hardware timer.
-# timer = Timer(0)
-
-# Setup the button input pin with a pull-up resistor.
-# button = Pin(0, Pin.IN, Pin.PULL_UP)
-
-# Register an interrupt on rising button input.
-# button.irq(debounce, Pin.IRQ_RISING)
-
 
 class Button:
 
@@ -28,8 +12,8 @@ class Button:
     def debounce(self, irq):
         self.timer.init(mode=Timer.ONE_SHOT, period=650, callback=self.do)
 
-    def do(self, param):
-        self._action()
+    def do(self, param=None):
+        self._action(param)
 
     def set_action(self, method):
         self._action = method
