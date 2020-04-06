607d034db1270464eae6a84788563962bf73399d
Javier Cruceño <cruceno.jm@gmail.com>
2020-04-06 08:56:23 -0300

    Se añade script para administracion de materiales

---
+++ materials.py
@@ -0,0 +1,31 @@
+import json
+
+class Material(object):
+    def __init__(self,
+                 code,
+                 name,
+                 curve,
+                 y0,
+                 slope,
+                 t_coef,
+                 c_coef,
+                 date):
+
+        self.code = code
+        self.name = name
+        self.curve = curve
+        self.y0 = y0
+        self.slope = slope
+        self.t_coef = t_coef
+        self.c_coef = c_coef
+        self.updated = date
+
+
+def material_from_json(json):
+    #TODO: Convertir material a objeto json
+    pass
+
+
+def material_to_json(material):
+    #TODO crear objeto json desde material
+    pass
