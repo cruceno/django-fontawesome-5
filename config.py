ee607c4bc4cc66222fcc82387699ec07f658d296
Javier Cruceño <cruceno.jm@gmail.com>
2020-04-08 20:19:47 -0300

    Se implementa sistema de configuracion
    Se crean archivos de configuracion y se implementan funciones basicas
    para su actualizacion y recuperacion.

--- config.py
+++ config.py
@@ -1,1 +1,53 @@
 import ujson
+
+
+def load_config():
+    try:
+        with open("config.json", 'rt') as f:
+            configuration = ujson.load(f)
+            return configuration
+
+    except Exception as e:
+        print(str(e))
+
+
+def save_config(config):
+    old_config = load_config()
+    try:
+        with open("config.json", 'w') as f:
+            new_config = old_config.update(config)
+            ujson.dump(new_config, f)
+
+    except Exception as e:
+        print(str(e))
+
+
+def load_defaults():
+    try:
+        with open("default.json", 'rt') as f:
+            configuration = ujson.load(f)
+            save_config(configuration)
+            return configuration
+
+    except Exception as e:
+        print(str(e))
+
+
+def update_config(old, new):
+    config = {}
+    for key, value in new.items():
+        if key in old:
+            if isinstance(value, dict):
+                config[key] = {}
+                for k, v in value.items():
+                    if k in old[key]:
+                        config[key][k] = v
+                    else:
+                        return {"result": 404, "msg": "Keyword {} not found".format(k)}
+            else:
+                config[key] = value
+        else:
+            return {"result": 404, "msg": "Keyword {} not found".format(key)}
+    old.update(config)
+    return {"result": 200, "msg": "Configuracion actualizada"}
+
