95ab5e2a0443d01fdf69c1a855f34d797518a11e
Javier Cruceño <cruceno.jm@gmail.com>
2020-04-23 00:20:42 -0300

    Se agregan funciones basicas para el manejo de los materiales

--- materials.py
+++ materials.py
@@ -1,31 +1,66 @@
-import json
+import ujson
+import uos
+MATERIAL_PATH = "data/materials"
+
 
 class Material(object):
-    def __init__(self,
-                 code,
-                 name,
-                 curve,
-                 y0,
-                 slope,
-                 t_coef,
-                 c_coef,
-                 date):
-
-        self.code = code
-        self.name = name
-        self.curve = curve
-        self.y0 = y0
-        self.slope = slope
-        self.t_coef = t_coef
-        self.c_coef = c_coef
-        self.updated = date
+    code = str()
+    name = str()
+    hum_rf = list()
+    y0 = int()
+    slope = int()
+    t_coef = int()
+    c_coef = int()
+    date = str()
 
 
-def material_from_json(json):
-    #TODO: Convertir objeto json s , material
-    pass
+def get_iterator():
+    return uos.ilistdir(MATERIAL_PATH)
+
+def material_from_code(code):
+    obj = Material()
+    materials = get_iterator()
+    for material in materials:
+        if code == material[0].split('.')[0]:
+            with open('/'.join([MATERIAL_PATH, material[0]]), 'r') as f:
+                data = ujson.load(f)
+                obj.code = data["code"]
+                obj.name = data["name"]
+                obj.hum_rf = data["hum_rf"]
+                obj.c_coef = data["c_coef"]
+                obj.t_coef = data["t_coef"]
+                obj.slope = data["slope"]
+                obj.y0 = data["y0"]
+                return obj
+    return None
 
 
-def material_to_json(material):
-    #TODO crear objeto json desde material
-    pass
+def remove_material(code):
+    materials = get_iterator()
+    for material in materials:
+        if code == material[0].split('.')[0]:
+            uos.remove('/'.join([MATERIAL_PATH, material[0]]))
+            return True
+    return False
+
+
+def add_material(material_dict):
+
+    try:
+        uos.stat('/'.join([MATERIAL_PATH, material_dict['code']]))
+        return False
+
+    except OSError:
+        with open('/'.join([MATERIAL_PATH, material_dict['code']+'.json']), 'w') as f:
+            ujson.dump(material_dict, f)
+            return True
+
+
+def update_material(material_dict):
+    materials = get_iterator()
+    for material in materials:
+        if material_dict['code'] == material[0].split('.')[0]:
+            with open('/'.join([MATERIAL_PATH, material_dict['code']+'.json']), 'w') as f:
+                ujson.dump(material_dict, f)
+                return True
+    return False
